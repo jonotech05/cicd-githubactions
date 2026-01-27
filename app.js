ffa6374a414830784f9acfc158aa034baaf53e60
jono nyams <jono@tech.io>
2026-01-27 15:22:27 -0800

    lint test locallly

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/health', (req, res) => {
 
 app.get('/api/info', (req, res) => {
     res.json({
-        message: 'Hello from CI/CD Lab!',
+        message: 'Hello from jonotech on CI/CD Lab!',
         environment: process.env.NODE_ENV || 'development',
         version: process.env.APP_VERSION || '1.0.0'
     });
